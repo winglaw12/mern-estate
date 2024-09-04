9dc95393acba798d757c6590eefe0daf2c560aea
Wing Law <winghlaw.com>
2024-09-04 12:05:26 -0700

    Create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
+
 dotenv.config()
 
 mongoose.connect(process.env.MONGO)
@@ -14,10 +16,11 @@ mongoose.connect(process.env.MONGO)
 
 const app = express()
 
+app.use(express.json())
+
 app.listen(3000, ()=> {
     console.log('Server running on port 3000');
 })
 
-app.use('/api/user', userRouter
-    
-)
+app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter)
