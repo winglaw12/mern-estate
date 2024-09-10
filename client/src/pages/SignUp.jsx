547ee832aa4b0622f725bd60fd6696a57db60f6b
Wing Law <winghlaw.com>
2024-09-09 20:41:48 -0700

    Complete sign up page functinality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,16 +1,57 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState } from 'react' 
+import {Link, useNavigate} from 'react-router-dom'
 
 export const SignUp = () => {
+  const [formData, setFormData] = useState({})
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
+  const handleChange = (e) => {
+    setFormData(
+      {
+        ...formData,
+        [e.target.id]: e.target.value,
+      }
+    )
+  }
+  const handleSubmit = async(e) => {
+    e.preventDefault()
+    try {
+          setLoading(true)
+          const res = await fetch('/api/auth/signup', 
+            {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(formData),
+            })
+            const data = await res.json()
+            console.log(data);       
+            if (data.success === false) {
+              setLoading(false)
+              setError(data.message)
+              return
+            }
+            setLoading(false)
+            setError(null)
+            navigate('/sign-in')
+        } catch (error) {
+        setLoading(false)
+        setError(error.message)
+        } 
+  }
+
+  
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form  className='flex flex-col gap-4'>
-        <input type="text" placeholder='username' className='border p03' id='username' />
-        <input type="text" placeholder='email' className='border p03' id='email' />
-        <input type="text" placeholder='password' className='border p03' id='password' />
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-50'>
-          Sign up
+      <form onSubmit={handleSubmit}  className='flex flex-col gap-4'>
+        <input type="text" placeholder='username' className='border p03' id='username' onChange={handleChange}/>
+        <input type="text" placeholder='email' className='border p03' id='email' onChange={handleChange}/>
+        <input type="text" placeholder='password' className='border p03' id='password' onChange={handleChange}/>
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-50'>
+          {loading? 'Loading...' : 'Sign Up'}
         </button>
       </form>
       <div className='flex gap-2 mt-5'>
@@ -19,6 +60,7 @@ export const SignUp = () => {
           <span className='text-blue-700'>Sign in</span>
          </Link>
       </div>
+      {error && <p className='text-red-500'>{error}</p>}
     </div>
   )
 }
