ff9ac9edfe6da905f3302fd34fb99b5cc7b9f8a8
Wing Law <winghlaw.com>
2024-09-24 16:35:49 -0700

    Add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { store } from './redux/store.js'
-import { Provider } from 'react-redux'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { persistor, store } from './redux/store.js';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
-)
+);
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,48 +1,48 @@
-import { useState } from 'react' 
-import { Link, useNavigate } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
 
 export const SignIn = () => {
-  const [formData, setFormData] = useState({})
-  const { loading, error } = useSelector((state) => state.user)
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
-    setFormData(
-      {
-        ...formData,
-        [e.target.id]: e.target.value,
-        
-      }
-    )
-  }
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
 
 
-  const handleSubmit = async(e) => {
-    e.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
-          dispatch(signInStart())
-          const res = await fetch('/api/auth/signin', 
-            {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify(formData),
-            })
-            const data = await res.json()
-            console.log(data);       
-            if (data.success === false) {
-              dispatch(signInFailure(data.message))
-              return
-            }
-            dispatch(signInSuccess(data))
-            navigate('/')
-        } catch (error) {
-        dispatch(signInFailure(error.message))
-        } 
-  }
+      dispatch(signInStart());
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+        return;
+      }
+      dispatch(signInSuccess(data));
+      navigate('/');
+    } catch (error) {
+      dispatch(signInFailure(error.message));
+    }
+  };
 
   
   return (
